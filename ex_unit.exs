dc2c1523bfca05b89172db1a042c5981882670be
brandonduff <bduff@mavenlink.com>
2018-11-30 14:10:15 -0700

    Work through TDD xUnit example a bit
    - Add script to watch tests
    - Ignore swap file

--- ex_unit.exs
+++ ex_unit.exs
@@ -1,26 +1,44 @@
 Code.require_file("Assert.exs")
 
 defmodule Test do
-  def init(test_method) do
-    %{ was_run: false, test_method: test_method }
+  def init(test_module) do
+    %{ was_run: false, was_set_up: false, test_module: test_module }
   end
 
   def run(test, args) do
-    apply(test.test_method, args)
+    result = apply(test.test_module.set_up, args)
+    apply(test.test_module.test, [result])
+  end
+end
+
+defmodule WasRun do
+  def set_up do
+    fn result -> %{ result | was_set_up: true, was_run: false } end
   end
 
+  def test do
+    fn result -> %{ result | was_run: true } end
+  end
 end
 
 defmodule TestCaseTest do
   import Assert
 
   def test_running do
-    test = Test.init(fn test -> %{ test | was_run: true } end)
+    test = Test.init(WasRun)
     assert(!test.was_run)
     result = Test.run(test, [test])
     assert(result.was_run)
     IO.puts "Success!"
   end
+
+  def test_set_up do
+    test = Test.init(WasRun)
+    result = Test.run(test, [test])
+    assert(result.was_set_up)
+    IO.puts "Success!"
+  end 
 end
 
 TestCaseTest.test_running()
+TestCaseTest.test_set_up()
---
+++ watch_ex_unit.sh
@@ -0,0 +1,1 @@
+fswatch -0 ./ex_unit.exs | xargs -0 -n1 elixir ./ex_unit.exs
